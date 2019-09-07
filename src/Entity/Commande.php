9a2e527793c5085727bc870f2d0503b80c1f9a5c
rajaomariajaona <rajaomariajaona11@gmail.com>
2019-09-07 13:39:40 +0300

    ajout champ date dans commande

--- src/Entity/Commande.php
+++ src/Entity/Commande.php
@@ -7,7 +7,7 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * Commande
  *
- * @ORM\Table(name="commande", indexes={@ORM\Index(name="fk_commande_etat_comande1", columns={"etat_commande"}), @ORM\Index(name="commande_ibfk_1", columns={"client_num_client"})})
+ * @ORM\Table(name="commande", indexes={@ORM\Index(name="commande_ibfk_1", columns={"client_num_client"}), @ORM\Index(name="fk_commande_etat_comande1", columns={"etat_commande"})})
  * @ORM\Entity(repositoryClass="App\Repository\CommandeRepository")
  */
 class Commande
@@ -20,6 +20,27 @@ class Commande
      * @ORM\GeneratedValue(strategy="IDENTITY")
      */
     private $numCommande;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date_commande", type="date", nullable=false)
+     */
+    private $dateCommande;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date_livraison", type="date", nullable=false)
+     */
+    private $dateLivraison;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date_paiement", type="date", nullable=false)
+     */
+    private $datePaiement;
 
     /**
      * @var \Client
@@ -44,6 +65,42 @@ class Commande
     public function getNumCommande(): ?int
     {
         return $this->numCommande;
+    }
+
+    public function getDateCommande(): ?\DateTimeInterface
+    {
+        return $this->dateCommande;
+    }
+
+    public function setDateCommande(\DateTimeInterface $dateCommande): self
+    {
+        $this->dateCommande = $dateCommande;
+
+        return $this;
+    }
+
+    public function getDateLivraison(): ?\DateTimeInterface
+    {
+        return $this->dateLivraison;
+    }
+
+    public function setDateLivraison(\DateTimeInterface $dateLivraison): self
+    {
+        $this->dateLivraison = $dateLivraison;
+
+        return $this;
+    }
+
+    public function getDatePaiement(): ?\DateTimeInterface
+    {
+        return $this->datePaiement;
+    }
+
+    public function setDatePaiement(\DateTimeInterface $datePaiement): self
+    {
+        $this->datePaiement = $datePaiement;
+
+        return $this;
     }
 
     public function getClientNumClient(): ?Client
